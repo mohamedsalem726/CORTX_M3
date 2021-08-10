80058d26990348dcbc88faaa2cb963e5e27c8e67
mohamedsalem726 <m_hsalem020@hotmail.com>
2021-08-10 14:34:59 +0200

    Update STD_TYPES.h

--- 01-LIB/01-STD_TYPES/STD_TYPES.h
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -1,7 +1,7 @@
 /**********************************************/
 /* Auther   : Mohamed Salem                   */
-/* Date     : 29 APRIL 2021                   */ 
-/* Version  : V02                             */
+/* Date     : 10 August 2021                  */ 
+/* Version  : V03                             */
 /**********************************************/
 #ifndef STD_TYPES_H
 #define STD_TYPES_H
@@ -10,9 +10,11 @@ typedef unsigned char        u8 ;
 typedef unsigned short int   u16;
 typedef unsigned long  int   u32;
 
-typedef signed char        s8 ;
-typedef signed short int   s16;
-typedef signed long  int   s32;
+typedef signed char          s8 ;
+typedef signed short int     s16;
+typedef signed long  int     s32;
 
+typedef float                f32;
+typedef double               f64;
 
 #endif
